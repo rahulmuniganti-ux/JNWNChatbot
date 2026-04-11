7404db89b753853ee293c2bb84b8b725789f7067
rahulmuniganti-ux <rahulmuniganti75@gmail.com>
2026-04-11 22:21:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ def delete(index):
 # Run App
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
